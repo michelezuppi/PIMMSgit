f131c709b9ce25099849175417d9526605e07778
michelezuppi <m.z.zuppi@amsterdamumc.nl>
2026-04-21 15:16:15 +0200

    vContact3 code adjustment

--- code/vcontact3.sh
+++ code/vcontact3.sh
@@ -4,16 +4,11 @@ input_folder=~/PIMMSgit/data/
 output_folder=~/PIMMSgit/data/results/
 #number of threads
 threads=16
-#runtime
-start=$SECONDS
 
-vcontact3 run \
-  --nucleotide "$input_folder/phages/all_species_representatives.fna" \
+{ time vcontact3 run \
+  --nucleotide "$input_folder/all_species_representatives.fna" \
   --output "$output_folder/vcontact3" \
   --db-domain "prokaryotes" \
-  --db-version 230 \
+  --db-version 228 \
   --db-path "$input_folder/databases/virus-host-db" \
-  --threads "$threads"
-
-elapsed=$((SECONDS - start ))
-echo "Runtime: $(( elapsed / 60 )) minutes and $(( elapsed % 60 )) seconds" > "$output_folder/vcontact3/runtime.txt"
+  --threads "$threads" ; } 2> "$output_folder/vcontact3/runtime.txt"
